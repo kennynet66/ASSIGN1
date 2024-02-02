ec79dd0eb82ea8f2b41590fd41ee1d4f3b7a7a05
Kenedy <kennynet66@gmail.com>
2024-02-02 11:51:59 +0300

    notify first commit

---
+++ assessment4/dist/index.js
@@ -0,0 +1,97 @@
+"use strict";
+// Load the HTML Elements
+let noteForm = document.querySelector('.note-form');
+let noteTitle = document.querySelector('#note-title');
+let note = document.querySelector('#note');
+let displayNotes = document.querySelector('#display');
+// Load the errors
+let noteError = document.querySelector('.error');
+let noteArr = [];
+// Load the local storage
+window.onload = () => {
+    let data = save.getNotes();
+    console.log(data);
+    if (data) {
+        data.forEach((el) => {
+            noteArr.push(el);
+        });
+        display.displayNote();
+    }
+    else {
+        display.displayNote();
+    }
+};
+noteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let verify = noteTitle.value.trim() != "" &&
+        note.value.trim() != "";
+    console.log(verify);
+    if (verify) {
+        let newNote = {
+            id: noteArr.length + 1,
+            title: noteTitle.value.trim(),
+            note: note.value.trim()
+        };
+        noteArr.push(newNote);
+        save.saveNote();
+        display.displayNote();
+    }
+    else {
+        alert("Please fill in all the notes fields");
+    }
+});
+class localSaves {
+    saveNote() {
+        localStorage.setItem('notifyNote', JSON.stringify(noteArr));
+    }
+    getNotes() {
+        let data = localStorage.getItem('notifyNote');
+        return JSON.parse(data);
+    }
+}
+let save = new localSaves;
+class displays extends localSaves {
+    displayNote() {
+        displayNotes.textContent = "";
+        if (noteArr.length >= 1) {
+            noteArr.forEach((note, index) => {
+                let card = document.createElement('div');
+                card.className = 'card';
+                let noteTitle = document.createElement('h4');
+                noteTitle.className = 'note-title';
+                noteTitle.textContent = note.title;
+                let noteId = document.createElement('p');
+                noteId.textContent = `ID: ${note.id}`;
+                let noteCont = document.createElement('p');
+                noteCont.textContent = note.note;
+                let buttons = document.createElement('div');
+                buttons.className = 'buttons';
+                let del = document.createElement('div');
+                del.className = 'delete';
+                del.textContent = "Delete";
+                del.addEventListener('click', () => {
+                    this.deleteNote(index);
+                });
+                let edit = document.createElement('div');
+                edit.className = "edit";
+                edit.textContent = "Edit";
+                buttons.appendChild(del);
+                buttons.appendChild(edit);
+                card.appendChild(noteTitle);
+                card.appendChild(noteId);
+                card.appendChild(noteCont);
+                card.appendChild(buttons);
+                displayNotes.appendChild(card);
+            });
+        }
+        else {
+            displayNotes.textContent = "You have no Notes";
+        }
+    }
+    deleteNote(note) {
+        noteArr.splice(note, 1);
+        this.saveNote();
+        this.displayNote();
+    }
+}
+let display = new displays;
