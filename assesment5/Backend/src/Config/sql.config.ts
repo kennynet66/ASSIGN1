7c58b611c809aa490bede0f52049d00856b5f5c9
Kenedy <kennynet66@gmail.com>
2024-02-09 11:50:06 +0300

    Working .env

--- assesment5/Backend/src/Config/sql.config.ts
+++ assesment5/Backend/src/Config/sql.config.ts
@@ -1,8 +1,12 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const sqlConfig = {
-    user: "sa",
-    database: "notify",
-    password: "Boomplay@1",
-    server: "DESKTOP-5DPD39H\\MSSQLSERVER1",
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PWD,
+    server: process.env.SERVER,
     pool: {
         max: 10,
         min: 0,
